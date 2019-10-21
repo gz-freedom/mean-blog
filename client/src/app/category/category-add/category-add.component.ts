9c756b819a67e446180d6b749680ca6b35d31dee
gz-freedom <myec2005@163.com>
2019-10-21 11:17:27 +0800

    Category CRUD

--- client/src/app/category/category-add/category-add.component.ts
+++ client/src/app/category/category-add/category-add.component.ts
@@ -18,10 +18,36 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./category-add.component.scss']
 })
 export class CategoryAddComponent implements OnInit {
+  categoryForm: FormGroup;
+  catName = '';
+  catDesc = '';
+  catImgUrl = '';
+  catContent = '';
+  isLoadingResults = false;
+  matcher = new MyErrorStateMatcher();
 
-  constructor() { }
+  constructor(private router: Router, private api: CategoryService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.categoryForm = this.formBuilder.group({
+      catName: [null, Validators.required],
+      catDesc: [null, Validators.required],
+      catImgUrl: [null, Validators.required],
+      catContent: [null, Validators.required]
+    });
+  }
+
+  onFormSubmit() {
+    this.isLoadingResults = true;
+    this.api.addCategory(this.categoryForm.value)
+      .subscribe((res: any) => {
+        const id = res._id;
+        this.isLoadingResults = false;
+        this.router.navigate(['/category-details', id]);
+      }, (err: any) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
   }
 
 }
